9b8eca2e5bc015e910b829f81e69baf86c2b472e
yocol-yc <chuan.yan@outlook.comgit config --global user.name yocol-ycgit config --global user.email chuan.yan@outlook.com>
2026-06-01 17:33:24 +0200

    changed the README and added gitignore for the file jump not done yet

--- source/dodo_rl/dodo_rl/tasks/locomotion/__init__.py
+++ source/dodo_rl/dodo_rl/tasks/locomotion/__init__.py
@@ -49,3 +49,28 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DodoFlatPPORunnerCfg",
     },
 )
+
+##
+# Jump
+##
+"""
+gym.register(
+    id="Dodo-Jump-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.jump_env_cfg:DodoJumpEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DodoFlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Dodo-Velocity-Flat-Play-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.flat_env_cfg:DodoFlatEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:DodoFlatPPORunnerCfg",
+    },
+)
+"""
